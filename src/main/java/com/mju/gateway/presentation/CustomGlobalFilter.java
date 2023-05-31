4e34b4dc8e62612728cd079a15d3cbde2b776f1e
Yunsik-Choi <choiys0311@gmail.com>
2023-05-31 19:16:16 +0900

    feat : authentication 기능 구현

---
+++ src/main/java/com/mju/gateway/presentation/CustomGlobalFilter.java
@@ -0,0 +1,75 @@
+package com.mju.gateway.presentation;
+
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class CustomGlobalFilter implements GlobalFilter, Ordered {
+    private static final String SOCOA_SSO_TOKEN = "SOCOA-SSO-TOKEN";
+    private static final String FIRST_LOGIN_PATH = "/user-service/login";
+    private static final String LOGIN_URI = "http://login.socoa.online/user/login";
+    private static final String SOCOA_HOME_URI = "http://socoa.online";
+
+    @Override
+    public Mono<Void> filter(final ServerWebExchange exchange, final GatewayFilterChain chain) {
+        try {
+            log.info("global filter = {}", exchange.getRequest().getURI());
+            MultiValueMap<String, HttpCookie> cookies = exchange.getRequest().getCookies();
+
+            if (isUnAuthenticationRequest(cookies)) {
+                return unAuthenticationRequest(exchange);
+            }
+
+            if (isFirstLogin(exchange)) {
+                log.info("first login = {} token = {}", exchange.getRequest().getURI(), cookies.get(SOCOA_SSO_TOKEN));
+                return firstLoginRequest(exchange);
+            }
+
+            return chain.filter(exchange);
+        } catch (Exception e) {
+            log.error("exception {}", exchange.getRequest().getURI());
+            e.printStackTrace();
+            return unAuthenticationRequest(exchange);
+        }
+    }
+
+    private static boolean isUnAuthenticationRequest(final MultiValueMap<String, HttpCookie> cookies) {
+        return cookies.containsKey(SOCOA_SSO_TOKEN) == false;
+    }
+
+    private static boolean isFirstLogin(final ServerWebExchange exchange) {
+        return exchange.getRequest().getURI().getPath().equals(FIRST_LOGIN_PATH);
+    }
+
+    private Mono<Void> firstLoginRequest(final ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.PERMANENT_REDIRECT);
+        response.getHeaders().setLocation(URI.create(SOCOA_HOME_URI));
+        return response.setComplete();
+    }
+
+    private Mono<Void> unAuthenticationRequest(final ServerWebExchange exchange) {
+        log.warn("UnAuthenticationRequest = {}", exchange.getRequest().getURI());
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.PERMANENT_REDIRECT);
+        response.getHeaders().setLocation(URI.create(LOGIN_URI));
+        return response.setComplete();
+    }
+
+    public int getOrder() {
+        return -1;
+    }
+}
